71d04941bb15acea1c93ef04e60adf42cadd86df
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-19 19:55:38 +0530

    Create pages for website

---
+++ src/pages/pages/about.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function about() {
+    return (
+        <div className='container' role="document">
+            <section>
+                <div className="headings">
+                <h2>About us</h2>
+                    <h3>
+                        My name Rajdeep singh
+                    </h3>
+                </div>
+
+                <h6>Hi welcome, </h6>
+
+                <p>
+                    I completed my study in master of science in biotechnology last year. After passing out 12 classes, I brought MY first android mobile phone. Then I started using mobile, and I found a video on youtube about how to earn online money. that video changed my life. During my B.sc, I successfully found my first job on the internet. I got selected for a front-end developer role on just theme. My part is to convert png design into HTML and CSS.
+                </p>
+                <p>
+                    After one year, I changed my job. In my second job, I learn many things like reactjs, API, CMS, etc.
+                </p>
+                <p>
+
+                    I started my writing career as a technical writer during my job. I published my first book on Linux and a research paper with osf. Some later, I publish one more book on bioinformatics.
+
+                </p>
+                <p>
+
+                    Now I publish an article every week on medium and other publications. You can read my article on my medium profile.
+                </p>
+                <p>Thank for reading</p>
+            </section>
+        </div>
+    )
+}
+
+export default about;
---
+++ src/pages/pages/contact.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function contact() {
+    return (
+        <div className='container'>
+            <h2> Contact us</h2>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis perferendis officia tempora cumque doloribus voluptatem sit voluptatibus sequi perspiciatis doloremque.
+            </p>
+            <p>
+                Veritatis blanditiis alias corporis ipsum placeat fugiat ab dolores vitae.
+            </p> 
+
+            <p>Thank for contact me. </p>
+
+        </div>
+    )
+}
+
+export default contact;
