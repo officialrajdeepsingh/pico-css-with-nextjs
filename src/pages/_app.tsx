c39c541f91200b4966ab349e99b7b05bfd9f3eaf
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-19 19:52:54 +0530

    Create the layout for blog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
+import '@picocss/pico'
 import type { AppProps } from 'next/app'
-
+import Header from 'components/Header';
+import '../style/style.css';
+import Footer from 'components/Footer';
+import Script from 'next/script';
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Header />
+    <Component {...pageProps} />
+    <Footer/>
+    <Script src='/theme.js'/>
+  </>
+
+
 }
