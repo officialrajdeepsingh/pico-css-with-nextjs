635b07209c188ab89908689bfd5ecd02c0bd0e04
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-20 23:18:54 +0530

    Add the  dark and light functionality for blog

--- public/theme.js
+++ public/theme.js
@@ -1,12 +1,9 @@
 var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
-console.log("click me outer side ");
 
 if (document.documentElement.getAttribute("data-theme") === "dark") {
-
     themeToggleDarkIcon.classList.remove('hidden');
-
 }else{
     themeToggleLightIcon.classList.remove('hidden');
 }
@@ -17,18 +14,15 @@ var themeToggleBtn = document.getElementById('theme-toggle');
 
 
 themeToggleBtn.addEventListener('click', function() {
-
-   // toggle icons inside button
    
    themeToggleDarkIcon.classList.toggle('hidden');
    themeToggleLightIcon.classList.toggle('hidden');
 
 
-    console.log('click me');
-
-    if (document.documentElement.getAttribute("data-theme") === "dark") {
+    if (!themeToggleLightIcon.classList.contains("hidden")) {
         document.documentElement.setAttribute('data-theme','dark');
     }else{
-        document.documentElement.setAttribute('data-theme','light')
+        document.documentElement.setAttribute('data-theme','light');
     }
+
 });
