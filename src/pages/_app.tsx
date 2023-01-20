b00c2ae8b33719dd6f2d5c2bf6c0e82a87f1ab5d
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-20 22:45:37 +0530

    update the _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,28 @@
 import '@picocss/pico'
+
+/*
+
+Enable custom theme
+
+import '../style/custom.css';
+import '../../poco.css';
+
+*/
+
+import '../style/style.css';
+
 import type { AppProps } from 'next/app'
 import Header from 'components/Header';
-import '../style/style.css';
 import Footer from 'components/Footer';
 import Script from 'next/script';
+
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-    <Header />
-    <Component {...pageProps} />
-    <Footer/>
-    <Script src='/theme.js'/>
-  </>
+            <Header />
+            <Component {...pageProps} />
+            <Footer/>
+            <Script src='/theme.js'/>
+        </>
 
 
 }
