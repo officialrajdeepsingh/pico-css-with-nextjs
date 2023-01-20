dd39032444ee230f6bed181368e393b06d73e6a8
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-20 23:41:23 +0530

    Remove the Script components in nextjs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script';
 export default function Document() {
   return (
     <Html lang="en" data-theme="dark">
@@ -8,7 +7,6 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-      <Script src="/theme.js" />
     </Html>
   )
 }
